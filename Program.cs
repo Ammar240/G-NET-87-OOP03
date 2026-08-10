1ca5fdac74fe0a5e747ae7077ec39d5018a14b1a
Ammar <eammar240@gmail.com>
2026-08-10 16:23:21 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_session_3_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
